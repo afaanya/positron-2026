aa86ef286cc8bcf437398c609b0938afa9a1c291
afaanya <shfharis@gmail.com>
2026-06-03 04:23:47 +0700

    controller

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::view('/', 'homepage');
-Route::view('/filosofi', 'filosofi');
-Route::view('/timeline', 'timeline');
-Route::view('/group', 'group');
-Route::view('/contact', 'contact');
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\FilosofiController;
+use App\Http\Controllers\TimelineController;
+use App\Http\Controllers\GroupController;
+use App\Http\Controllers\ContactController;
+
+Route::get('/', [HomeController::class, 'index']);
+Route::get('/filosofi', [FilosofiController::class, 'index']);
+Route::get('/timeline', [TimelineController::class, 'index']);
+Route::get('/group', [GroupController::class, 'index']);
+Route::get('/contact', [ContactController::class, 'index']);
