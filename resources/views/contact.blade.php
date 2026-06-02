06c67178823fac07c1403e0815f316febb6be620
afaanya <shfharis@gmail.com>
2026-06-03 06:01:37 +0700

    view

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,5 +1,98 @@
 @extends('layouts.app')
 
 @section('content')
-<h1>Contact</h1>
-@endsection
+
+<section>
+    <h1 class="font-primary">
+        KONTAK MENTOR POSITRON 2026
+    </h1>
+    <p class="font-secondary">
+        Jika Anda memiliki pertanyaan atau ingin menghubungi mentor Positron 2026, <br>
+        silakan gunakan informasi kontak berikut:
+    </p>
+</section>
+
+<section>
+    <h2 class="font-primary">Contact Person Program Studi</h2>
+    <ul class="font-secondary">
+        <li><strong>Program Studi Teknik Informatika</strong></li>
+        <li><strong>Nama:</strong>aaaaa</li>
+        <li><strong>Nomor HP:</strong> 081234567890</li>
+    </ul>
+
+    <ul class="font-secondary">
+        <li><strong>Program Studi Pendidikan Teknik Informatika</strong></li>
+        <li><strong>Nama:</strong>bbbbb</li>
+        <li><strong>Nomor HP:</strong> 081234567891</li>
+    </ul>   
+
+    <ul class="font-secondary">
+        <li><strong>Program Studi Teknik Elektro</strong></li>
+        <li><strong>Nama:</strong>ccccc</li>
+        <li><strong>Nomor HP:</strong> 081234567892</li>
+    </ul>  
+
+    <ul class="font-secondary">
+        <li><strong>Program Studi Pendidikan Teknik Elektro</strong></li>
+        <li><strong>Nama:</strong>ddddd</li>
+        <li><strong>Nomor HP:</strong> 081234567893</li>
+    </ul>
+</section>
+
+<section>
+    <h2 class="font-primary">Contact Person Kegiatan Positron</h2>
+    <ul class="font-secondary">
+        <li><strong>FORUM MABA</strong></li>
+        <li><strong>Nama:</strong>eeeee</li>
+        <li><strong>Nomor HP:</strong> 081234567894</li>
+    </ul>
+
+    <ul class="font-secondary">
+        <li><strong>LDK</strong></li>
+        <li><strong>Nama:</strong>fffff</li>
+        <li><strong>Nomor HP:</strong> 081234567895</li>
+    </ul>   
+
+    <ul class="font-secondary">
+        <li><strong>IoH</strong></li>
+        <li><strong>Nama:</strong>ggggg</li>
+        <li><strong>Nomor HP:</strong> 081234567896</li>
+    </ul>  
+
+    <ul class="font-secondary">
+        <li><strong>NAKO</strong></li>
+        <li><strong>Nama:</strong>hhhhh</li>
+        <li><strong>Nomor HP:</strong> 081234567897</li>
+    </ul>
+</section>
+
+<section>
+    <h2 class="font-primary">Contact Person Kakak Mentor</h2>
+    <ul class="font-secondary">
+        <li><strong>Nama:</strong>iiiii</li>
+        <li><strong>Program Studi:</strong> Teknik Informatika</li>
+        <li><strong>Offering:</strong>A</li>
+        <li><strong>Nomor HP:</strong> 081234567898</li>
+    </ul>
+
+    <ul class="font-secondary">
+        <li><strong>Nama:</strong>jjjjj</li>
+        <li><strong>Program Studi:</strong> Pendidikan Teknik Informatika</li>
+        <li><strong>Offering:</strong>A</li>
+        <li><strong>Nomor HP:</strong> 081234567899</li>
+    </ul>
+
+    <ul class="font-secondary">
+        <li><strong>Nama:</strong>kkkkk</li>
+        <li><strong>Program Studi:</strong> Teknik Elektro</li>
+        <li><strong>Offering:</strong>A</li>
+        <li><strong>Nomor HP:</strong> 081234567900</li>
+    </ul>  
+
+    <ul class="font-secondary">
+        <li><strong>Nama:</strong>lllll</li>
+        <li><strong>Program Studi:</strong> Pendidikan Teknik Elektro</li>
+        <li><strong>Offering:</strong>A</li>
+        <li><strong>Nomor HP:</strong> 081234567901</li>
+    </ul>
+</section>
--- resources/views/filosofi.blade.php
+++ resources/views/filosofi.blade.php
@@ -1,5 +1,27 @@
 @extends('layouts.app')
 
 @section('content')
-<h1>Filosofi</h1>
+
+<section id="hero">
+    <h1 class="font-primary">
+        POSITRON 2026
+    </h1>
+    <p class="font-secondary">
+        Filosofi Logo Positron 2026 Departemen Teknik Elektro dan Informatika Fakultas Teknik Universita Negeri Malang
+    </p>
+    <img src="" alt="logo positron">
+</section>
+
+<section id="makna-logo">
+    <h2 class="font-primary">
+        Makna Logo</h2>
+    <img src="" alt="makna logo">
+    <p class="font-secondary">
+        Logo Positron 2026 memiliki makna yang mendalam, <br>
+        mencerminkan semangat dan nilai-nilai yang ingin ditanamkan <br>
+        kepada mahasiswa baru Departemen Teknik Elektro dan Informatika. <br>
+        Berikut adalah penjelasan makna dari logo Positron 2026:
+    </p>
+</section>
+
 @endsection
