2d39c0a30caa937c834ec9a29031566ede12cd2b
afaanya <shfharis@gmail.com>
2026-06-20 19:43:31 +0700

    edit

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,21 +1,30 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="id">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>POSITRON 2026</title>
-    <link href="https://fonts.googleapis.com/css2?family=Libre+Baskerville:wght@400;700&display=swap" rel="stylesheet">
-
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <title>Website Maba</title>
 </head>
 <body>
-    <nav>
-        <a href="/">Home</a> 
-        <a href="/timeline">Timeline</a> 
-        <a href="/filosofi">Filosofi</a> 
-        <a href="/group">Group</a> 
-        <a href="/contact">Contact</a>
-    </nav>
-    @yield('content')
+
+@auth
+<nav>
+    <a href="/home">Home</a>
+    <a href="/about">About</a>
+    <a href="/filosofi">Filosofi</a>
+    <a href="/timeline">Timeline</a>
+    <a href="/contact">Contact</a>
+    <a href="/profil">Profil</a>
+
+    <form method="POST" action="{{ route('logout') }}" style="display:inline;">
+        @csrf
+        <button type="submit">Logout</button>
+    </form>
+</nav>
+@endauth
+
+<hr>
+
+@yield('content')
+
 </body>
 </html>
