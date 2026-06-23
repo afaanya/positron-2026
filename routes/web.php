db8a70dc5eaf708bc602c661bbe6afa89241718f
notspace0 <fina.najma.2505336@students.um.ac.id>
2026-06-23 15:27:41 +0700

    perbaikan

--- routes/web.php
+++ routes/web.php
@@ -3,34 +3,8 @@
 use Illuminate\Support\Facades\Route; 
 use App\Http\Controllers\Auth\LoginController;
 
-<<<<<<< HEAD
-Route::get('/', function () {
-    return redirect()->route('login');
-});
 
-Route::get('/home', function () {
-    return view('filosofi');
-});
-
-Route::middleware('guest')->group(function () {
-    Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
-    Route::post('/login', [LoginController::class, 'login']);
-});
-
-Route::post('/logout', [LoginController::class, 'logout'])
-    ->name('logout')
-    ->middleware('auth');
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware('auth')->name('dashboard');
-
-=======
-// Halaman landing (root)
-Route::get('/', function () {
-    return view('landing');
-})->name('home');
-
+// mahasiswa punya
 // Redirect jika sudah login
 Route::get('/', function () {
     return auth()->check()
@@ -38,8 +12,55 @@ Route::get('/', function () {
         : view('landing');
 })->name('home');
 
+// Home page
+Route::get('homepage', function(){
+    return view('mahasiswa.homepage');
+})->name('home');
+
+// Halaman about
+Route::get('/about', function () {
+    return view('about');
+});
+
+
+// Halaman profil mahasiswa
+Route::get('/profil-mahasiswa', function () {
+    return view('profil-mahasiswa');
+})->name('profil');
+
+Route::get('/biodata', function () {
+    $contact = '081234567890';
+    $biodata = (object) [
+        'nama' => 'abcdef',
+        'nim' => '123456789',
+        'program_studi' => 'Teknik Informatika',
+        'offering' => 'TI A',
+        'kakak_mentor' => 'klmnop',
+        'contact' => 'https://wa.me/62' .ltrim($contact, '0'),
+        'kelompok' => 'Kelompok 1',
+        'mentor_kelompok' => 'uvwxyz'
+    ];
+    return view('biodata-mahasiswa', compact('biodata'));
+});
+
+Route::get('/poin', function () {
+    return view('poin-penilaian-mahasiswa');
+});
+
+Route::get('/sertifikat', function () {
+    return view('sertifikat-mahasiswa');
+});
+
+// Halaman filosofi
+Route::get('/filosofi', function () {
+    return view('filosofi');
+})->name('filosofi');
+
 Route::middleware('guest')->group(function () {
-    Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
+    Route::get('/login', function () {
+        return view('auth.login');
+    })->name('login');
+
     Route::post('/login', [LoginController::class, 'login']);
 });
 
@@ -50,4 +71,26 @@ Route::post('/logout', [LoginController::class, 'logout'])
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware('auth')->name('dashboard');
->>>>>>> 6c87537de27a312355508fc7fc142aeb6236859e
+
+// Halaman Timeline
+Route::get('/timeline', function () {
+    return view('timeline');
+});
+
+
+// mentor punya
+Route::prefix('mentor')->group(function () {
+
+    Route::get('/kegiatan', function () {
+        return view('mentor.kegiatan');
+    })->name('mentor.prodi');
+
+    Route::get('/offering/{prodi}', function ($kegiatan) {
+        return view('mentor.offering', compact('kegiatan'));
+    })->name('mentor.offering');
+
+    Route::get('/mahasiswa', function () {
+        return view('mentor.mahasiswa');
+    })->name('mentor.mahasiswa');
+
+});
