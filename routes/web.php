39a18b7162bf218165d21fb0ec2fc7f8d64cd4bb
alennevadaa-beep <alennevadaa@gmail.com>
2026-07-05 16:34:21 +0700

    au

--- routes/web.php
+++ routes/web.php
@@ -14,11 +14,6 @@ Route::get('/', function () {
     return view('landing');
 })->name('landing');
 
-// Simple Hello World route
-Route::get('/hello', function () {
-    return 'Hello World';
-})->name('hello');
-
 
 // ================= LOGIN =================
 
@@ -115,64 +110,9 @@ Route::get('/sertifikat', function () {
 Route::post('/logout', [LoginController::class, 'logout'])
     ->name('logout');
 
-
 // ================= HALAMAN SETELAH LOGIN =================
 
-// Route::middleware('mentor.auth')->group(function () {
-
-//     Route::get('/home', function () {
-//         return view('home');
-//     })->name('home');
-
-//     Route::get('/homepage', function () {
-//         return view('homepage');
-//     })->name('homepage');
-
-//     Route::get('/dashboard', function () {
-//         return view('dashboard');
-//     })->name('dashboard');
-
-//     Route::get('/sambutan', function () {
-//         return view('sambutan');
-//     })->name('sambutan');
-
-//     Route::get('/rangkaian', function () {
-//         return view('rangkaian');
-//     })->name('rangkaian');
-
-//     Route::get('/manualbook', function () {
-//         return view('manualbook');
-//     })->name('manualbook');
-
-//     Route::get('/about', function () {
-//         return view('about');
-//     })->name('about');
-
-//     Route::get('/filosofi', function () {
-//         return view('filosofi');
-//     })->name('filosofi');
-
-//     Route::get('/timeline', function () {
-//         return view('timeline');
-//     })->name('timeline');
-
-//     Route::get('/profil-mahasiswa', function () {
-//         return view('profil-mahasiswa');
-//     })->name('profil');
-
-//     Route::get('/biodata', function () {
-//         return view('biodata-mahasiswa');
-//     })->name('biodata');
-
-//     Route::get('/poin', function () {
-//         return view('poin-penilaian-mahasiswa');
-//     })->name('poin');
-
-//     Route::get('/sertifikat', function () {
-//         return view('sertifikat-mahasiswa');
-//     })->name('sertifikat');
-
-// });
+Route::middleware('mentor.auth')->group(function () {
 
     Route::get('/home', function () {
         return view('home');
@@ -214,6 +154,10 @@ Route::post('/logout', [LoginController::class, 'logout'])
         return view('profil-mahasiswa');
     })->name('profil');
 
+    Route::get('/biodata', function () {
+        return view('biodata-mahasiswa');
+    })->name('biodata');
+
     Route::get('/poin', function () {
         return view('poin-penilaian-mahasiswa');
     })->name('poin');
@@ -221,3 +165,5 @@ Route::post('/logout', [LoginController::class, 'logout'])
     Route::get('/sertifikat', function () {
         return view('sertifikat-mahasiswa');
     })->name('sertifikat');
+
+});
