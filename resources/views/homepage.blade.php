d372f1b3b138d185fc9d4a9a65e6261bfb4bf59a
alennevadaa-beep <alennevadaa@gmail.com>
2026-07-13 11:25:56 +0700

    homepagej

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -1,7 +1,5 @@
 @vite('resources/css/homepage.css')
-    <div class="hero">
-        <img src="{{ asset('images/selamat-datang.png') }}" class="selamat-datang">
-        </a>
-    </div>
-</div>
 
+<div class="hero">
+    <img src="{{ asset('images/selamat-datang.png') }}" class="selamat-datang">
+</div>
