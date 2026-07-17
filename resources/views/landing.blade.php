4f626439994c6da77f505ecf833fc226a3723fe5
afaanya <shfharis@gmail.com>
2026-07-17 19:09:33 +0700

    fix responsif landing lagi

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -12,9 +12,7 @@
         <img src="{{ asset('images/login-bg.png') }}" alt="Background" class="layer-bg">
 
         <!-- Amplop versi desktop (miring, posisi absolute kanan) -->
-        <a href="{{ route('login') }}" class="amplop-link amplop-link-desktop" aria-label="Login">
-            <img src="{{ asset('images/amplop-landing-miring.png') }}" alt="Amplop" class="layer-amplop layer-amplop-desktop">
-        </a>
+        <img src="{{ asset('images/amplop-landing-miring.png') }}" alt="Amplop" class="layer-amplop layer-amplop-desktop">
 
         <!-- Frame border emas -->
         <img src="{{ asset('images/frame-landing.png') }}" alt="Frame" class="layer-frame">
@@ -23,13 +21,17 @@
         <div class="content">
             <img src="{{ asset('images/invited-landing.png') }}" alt="You're Invited" class="layer-invited">
 
-            <!-- Amplop versi mobile: nyempil di antara invited & garis -->
-            <a href="{{ route('login') }}" class="amplop-link amplop-link-mobile" aria-label="Login">
+            <!-- Amplop versi mobile: nyempil di antara invited & garis, jadi link login khusus mobile -->
+            <a href="{{ route('login') }}" class="amplop-link-mobile" aria-label="Login">
                 <img src="{{ asset('images/amplop-landing.png') }}" alt="Amplop" class="layer-amplop layer-amplop-mobile">
             </a>
 
             <img src="{{ asset('images/garis-landing.png') }}" alt="Divider" class="layer-garis">
             <img src="{{ asset('images/special-landing.png') }}" alt="A special invitation awaits you" class="layer-special">
+
+            <a href="{{ route('login') }}" class="open-invitation" id="openInvitation" aria-label="Open Invitation">
+                <img src="{{ asset('images/open-landing.png') }}" alt="Open Invitation">
+            </a>
         </div>
     </div>
 </body>
