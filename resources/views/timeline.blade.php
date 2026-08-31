c65d777afa4ce46cff56063835f2dafa571ff1c2
NamaPenggunaGitHubAnda <emailAnda@example.com>
2026-08-31 13:10:59 +0700

    fix missing production vite assets

--- resources/views/timeline.blade.php
+++ resources/views/timeline.blade.php
@@ -199,7 +199,7 @@
     // urutan titik 1-4 di peta perjalanan (index 0 = titik 1, dst).
     const timelineEventsData = [
         { name: 'FORUM MABA 2026', startDate: new Date(2026, 7, 28), endDate: new Date(2026, 7, 29, 23, 59, 59),
-          link: 'https://drive.google.com/file/d/1sYtknCg0eoQnbcBHEmmU-oqG3ExEpxmS/view',
+          link: 'https://drive.google.com/file/d/1KWfd5JsHnJjvwCeEh8tCeJckvE5cWElZ/view',
           docLink: 'https://drive.google.com/drive/folders/1Cek-zy_533IfN2vN2wvsrK_TsdP0gRGl',
           structureLink: 'https://drive.google.com/file/d/1csReM4ADq4Dv59pl8PJ6QAo2L8CqALyG/view?usp=sharing' },
         { name: 'LDK 2026', startDate: new Date(2026, 9, 11), endDate: new Date(2026, 9, 11, 23, 59, 59),
