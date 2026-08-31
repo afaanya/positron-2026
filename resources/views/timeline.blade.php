b378b6bf97ba8eceb875030e6f2dfa7860605495
NamaPenggunaGitHubAnda <emailAnda@example.com>
2026-08-31 13:28:31 +0700

    fix missing production vite assets

--- resources/views/timeline.blade.php
+++ resources/views/timeline.blade.php
@@ -201,7 +201,7 @@
         { name: 'FORUM MABA 2026', startDate: new Date(2026, 7, 28), endDate: new Date(2026, 7, 29, 23, 59, 59),
           link: 'https://drive.google.com/file/d/1KWfd5JsHnJjvwCeEh8tCeJckvE5cWElZ/view',
           docLink: 'https://drive.google.com/drive/folders/1Cek-zy_533IfN2vN2wvsrK_TsdP0gRGl',
-          structureLink: 'https://drive.google.com/file/d/1csReM4ADq4Dv59pl8PJ6QAo2L8CqALyG/view?usp=sharing' },
+          structureLink: 'https://drive.google.com/file/d/1-QWK-ueRd2l5dCGRL8q1nz0omHr1D1zk/view?usp=sharing' },
         { name: 'LDK 2026', startDate: new Date(2026, 9, 11), endDate: new Date(2026, 9, 11, 23, 59, 59),
           link: 'https://link-manual-book-ldk.com',
           docLink: 'https://link-dokumentasi-ldk.com' },
